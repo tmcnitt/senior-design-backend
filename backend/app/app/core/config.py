8576515ddab91feb79292816dfebf442cba32522
Troy McNitt <tmcnitt@smu.edu>
2022-04-25 15:26:50 -0500

    actually fix CORS bug

--- backend/app/app/core/config.py
+++ backend/app/app/core/config.py
@@ -14,13 +14,7 @@ class Settings(BaseSettings):
     # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
     # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
     # "http://localhost:8080", "http://local.dockertoolbox.tiangolo.com"]'
-    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = [
-        "http://ec2-54-176-1-242.us-west-1.compute.amazonaws.com",
-        "http://172.23.0.4:80",
-        "http://localhost:80",
-        "base-project-com-backend-http@docker",
-        "*"
-    ]
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
 
     @validator("BACKEND_CORS_ORIGINS", pre=True)
     def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
